f64c893d39485f0a8fc7981f7c02a71a3cc0c951
Tushar Sahni <tusharsahni22@.com>
2023-04-10 13:59:01 +0530

    funconality increased

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,5 @@
 const UserModel = require("../src/Database/model");
-const express = require("express")
+
 
 
 const  addUser = async (req,res)=>{
@@ -7,13 +7,45 @@ const  addUser = async (req,res)=>{
     data.save() 
     res.send(data)
 };
-const  findUser =  (req,res)=>{
-    const string ="working"
- res.send(string)
+
+
+const  getUserByParams = async (req,res)=>{
+    const id =(req.params.id)
+    const requestedData = await UserModel.findById(id)    
+    res.send(requestedData)
 };
-const  updateUser =  (req,res)=>{
-    const string ="working"
- res.send(string)
+
+const  getUser = async (req,res)=>{
+    const {name ,address,gender,age, phone,sort} =req.query;
+    const _id =req.params.id
+    const quriedData ={}
+
+    if(name){
+        quriedData.name ={$regex: name,$options:"i"}
+    }
+    if(address){
+        quriedData.address ={$regex: address,$options:"i"}
+    }
+    if(gender){
+        quriedData.gender ={$regex: gender,$options:"i"}
+    }
+    if(age){
+        quriedData.age =age
+    }
+    if(phone){
+        quriedData.phone =phone
+    }
+
+    let apiData=  UserModel.find(quriedData)
+    if(sort){
+       const sortfix = sort.split(",").toString().replace(","," ")
+        apiData=apiData.sort(sortfix)
+    }
+
+        const requestedData = await apiData  
+        res.send(requestedData)
+    
+
 }
 
-module.exports = {addUser,findUser};
+module.exports = {addUser,getUser,getUserByParams};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const router = express.Router();
-const {addUser,findUser} = require("../controller/user")
+const {addUser,getUser,getUserByParams} = require("../controller/user")
 router.use(express.json())
  
 router.route("/addUserDetails").post(addUser)
-// router.route("/removeUserDetails").get(UserDetails)
-// router.route("/removeUserDetails").get(UserDetails)
+router.route("/getUserDetails/").get(getUser)
+router.route("/getUserDetails/:id").get(getUserByParams)
+
+
+
 
 
 module.exports =router;
